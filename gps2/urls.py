26d7659e40f6d243959aaa1aaadffad51455ef0e
Roberto <robertoruiz141189@gmail.com>
2018-02-05 14:38:21 -0600

    ok

--- gps2/urls.py
+++ gps2/urls.py
@@ -22,3 +22,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('gps.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
